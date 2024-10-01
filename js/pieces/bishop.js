1e6be47be2208cd8abfac0379d324af7c62dca56
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-10-01 12:21:59 +0530

    Fixed a bug in bishop

--- js/pieces/bishop.js
+++ js/pieces/bishop.js
@@ -5,37 +5,55 @@ var Bishop = function(config) {
 
 Bishop.prototype = new Piece({});
 
-Bishop.prototype.isValidPosition = function(targetPosition) {
-    // Convert current position to row and column
+Bishop.prototype.isValidPosition = function(targetPosition, board) {
     let currentCol = this.position.charAt(0);
     let currentRow = parseInt(this.position.charAt(1));
 
-    // Calculate the differences in rows and columns
     let colDifference = Math.abs(targetPosition.col.charCodeAt(0) - currentCol.charCodeAt(0));
     let rowDifference = Math.abs(targetPosition.row - currentRow);
 
-    // Bishop moves diagonally, so the row difference should equal the column difference
     if (colDifference === rowDifference) {
-        return true;
+        return this.isPathClear(targetPosition, board);
     }
 
-    // If the move is not diagonal, it's invalid for a bishop
     console.warn("Invalid move for bishop");
     return false;
 }
 
+Bishop.prototype.isPathClear = function(targetPosition, board) {
+    let currentCol = this.position.charAt(0);
+    let currentRow = parseInt(this.position.charAt(1));
+    let targetCol = targetPosition.col;
+    let targetRow = targetPosition.row;
+
+    let colStep = targetCol > currentCol ? 1 : -1;
+    let rowStep = targetRow > currentRow ? 1 : -1;
+
+    let col = currentCol.charCodeAt(0) + colStep;
+    let row = currentRow + rowStep;
+
+    while (col !== targetCol.charCodeAt(0) && row !== targetRow) {
+        let position = String.fromCharCode(col) + row;
+        if (board.getPieceAt({ col: String.fromCharCode(col), row })) {
+            return false; 
+        }
+        col += colStep;
+        row += rowStep;
+    }
+
+    return true; 
+}
+
 Bishop.prototype.moveTo = function(targetPosition, board) {
-    if (this.isValidPosition(targetPosition)) {
+    if (this.isValidPosition(targetPosition, board)) {
         const targetPiece = board.getPieceAt(targetPosition);
-        if(targetPiece && targetPiece.color !== this.color){
+        if (targetPiece && targetPiece.color !== this.color) {
             targetPiece.kill(targetPiece, board);
-            // return;
         } 
-        // Update the bishop's position to the new position
         this.position = targetPosition.col + targetPosition.row;
         this.render();
     } else {
-        // Invalid move, do nothing
+        alert("Invalid move for the bishop!");
+        console.warn("Move blocked or invalid for bishop");
     }
 }
-
